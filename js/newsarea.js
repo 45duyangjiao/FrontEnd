986377d9d9326d55151fb87a9424c2ae0cf5764f
wangdf <keane_bj@aliyun.com>
2017-07-11 19:01:44 +0800

    列表

--- js/newsarea.js
+++ js/newsarea.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 		},
 		success: function(data) {
 			console.log(data);
-//			var str = template("protocol-temp", {activition: data.Data.Data.result})
-//			$(".activationRecord").html(str);
+			var str = template("myChannel", {myChannel: data.Data.Data})
+			$(".myChannelmodule").html(str);
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) {
 			alert(XMLHttpRequest.status);
@@ -21,27 +21,6 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() {
-	var url = 'http://d2p7pk.natappfree.cc/News/GetMyChannelList';
-	$.ajax({
-		url: url,
-		dataType: 'json',
-		type: 'post',
-		data: {
-			UserId: 10002
-		},
-		success: function(data) {
-			console.log(data);
-//			var str = template("protocol-temp", {activition: data.Data.Data.result})
-//			$(".activationRecord").html(str);
-		},
-		error: function(XMLHttpRequest, textStatus, errorThrown) {
-			alert(XMLHttpRequest.status);
-			alert(XMLHttpRequest.readyState);
-			alert(textStatus);
-		}
-	});
-});
 
 
 
