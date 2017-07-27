a3334b3635dec0bcd5f7beaf370b49f5395a3955
maxh <wangdf>
2017-07-27 15:43:29 +0800

    pingliun

--- js/newsList.js
+++ js/newsList.js
@@ -112,17 +112,13 @@ $(document).ready(function() {
 					let id = $(this).attr('id'); // 获取id
 					let viewCount = $(this).attr('viewCount');
 					let commentCount=$(this).attr("commentCount");
-					console.log(id)
 					window.location.href = "newsDetailPage.html?id=" + id + "&commentCount="+commentCount;
 				});
-				
-				function cityname(cityname){
-					window.location.href="segue//type="+cityname;
-				}
-				var cityname="cityname";
+
 				if(typeid==5){
+					var cityname="cityname";
 					$(".BaiduMap").click(function(){
-						cityname(cityname);
+						window.location.href="segue://type="+cityname;
 					})
 				}
 			},
