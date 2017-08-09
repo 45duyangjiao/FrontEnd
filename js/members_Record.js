bc90f947a656ee119313178713acf1648c2c4a9b
zhaocx <2545833592@qq.com>
2017-08-09 09:55:04 +0800

    测试记录点击

--- js/members_Record.js
+++ js/members_Record.js
@@ -9,6 +9,7 @@ var user_Id = GetQueryString("user_Id");
 console.log(user_Id);
 
 $(document).ready(function() {
+	        alert('t');
 	var url = 'http://47.93.192.128:5001/Daoguan/Activity_Record';
 	$.ajax({
 		url: url,
@@ -25,15 +26,13 @@ $(document).ready(function() {
 			console.log(data);
 			var str = template("memberRecord", {activition: data.Data.Data})
 			$(".mui-table-view-member").html(str);
-
-			mui(".mui-table-view-cell").each(function(index,item){
-				item.addEventListener('tap',function(){
-					 let id=$(this).attr('id');// 获取id
-					 localStorage.setItem("daoId",id)
-			         window.location.href = "membersCont.html?id=" + id;
-				})
-				
-			})
+			$(".mui-table-view-cell").click(function () {
+		        let id=$(this).attr('id');// 获取id
+//		        console.log(id);
+		        localStorage.setItem("daoId",id)
+		
+		        window.location.href = "membersCont.html?id=" + id;
+		    })
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) {
 			alert(XMLHttpRequest.status);
