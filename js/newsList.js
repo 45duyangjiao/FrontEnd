1d35013a71a7c50d89df700a46bdf447409aab4c
maxh <wangdf>
2017-08-11 11:12:38 +0800

    no message

--- js/newsList.js
+++ js/newsList.js
@@ -1,20 +1,13 @@
 
 
 
-//	function GetQueryString(name) {
-//		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-//		var r = window.location.search.substr(1).match(reg);
-//		if(r != null) return unescape(r[2]);
-//		return null;
-//	}
-	
-	    function getQueryString(name) { 
-			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
-			var r = window.location.search.substr(1).match(reg); 
-			if (r != null) return unescape(r[2]); return null; 
-		} 
+	function GetQueryString(name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if(r != null) return unescape(r[2]);
+		return null;
+	}
 
-     
 	var userID = GetQueryString("userID");      
 //	var userID =10014;
     console.log(userID);
