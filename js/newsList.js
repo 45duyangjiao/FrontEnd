bec03bdbe7d29ffd908d335063053c1a64871414
maxh <wangdf>
2017-08-11 11:04:25 +0800

    no message

--- js/newsList.js
+++ js/newsList.js
@@ -1,12 +1,19 @@
 
 
 
-	function GetQueryString(name) {
-		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-		var r = window.location.search.substr(1).match(reg);
-		if(r != null) return unescape(r[2]);
-		return null;
-	}
+//	function GetQueryString(name) {
+//		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+//		var r = window.location.search.substr(1).match(reg);
+//		if(r != null) return unescape(r[2]);
+//		return null;
+//	}
+	
+	    function getQueryString(name) { 
+			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+			var r = window.location.search.substr(1).match(reg); 
+			if (r != null) return unescape(r[2]); return null; 
+		} 
+
      
 	var userID = GetQueryString("userID");      
 //	var userID =10014;
