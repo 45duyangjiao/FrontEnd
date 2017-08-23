e673616378608453f08a17c28a6d743f4dec8034
maxh <wangdf>
2017-08-23 14:24:49 +0800

    no message

--- js/GetGongyiNewsList.js
+++ js/GetGongyiNewsList.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+	function GetQueryString(name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if(r != null) return unescape(r[2]);
+		return null;
+	}
+	
+	var userID=GetQueryString("userID");
+	console.log(userID)
+	
 	var url = 'http://47.93.192.128:5001/News/GetGongyiNewsList';
 	$.ajax({
 		url: url,
@@ -14,7 +24,7 @@ $(document).ready(function() {
 		    $(".activationRecordLi").click(function () {
 		        var id=$(this).attr('id');// 获取id
 		        var commentCount=$(this).attr("commentCount")
-		        window.location.href = "newsContent.html?id=" + id+"&commentCount="+commentCount;
+		        window.location.href = "newsContent.html?id=" + id+"&commentCount="+commentCount+"&userID="+userID;
 		    })
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) {
