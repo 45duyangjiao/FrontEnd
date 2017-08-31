bba46667913032d92d74b16b78d8a68f6c60a1af
maxh <wangdf>
2017-08-31 16:57:35 +0800

    no message

--- js/training_con.js
+++ js/training_con.js
@@ -1,12 +1,21 @@
 
 $(document).ready(function() {
+	
+	function GetQueryString(name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if(r != null) return unescape(r[2]);
+		return null;
+	}
+	
+	var id = GetQueryString("id");
 	var url = 'http://47.93.192.128:5001/Center/ActivityDetail';
 	$.ajax({
 		url: url,
 		dataType: 'json',
 		type: 'post',
 		data:{
-			ActivityId:localStorage.getItem("ActivityId"),
+			ActivityId:id,
 		},
 		success: function(data) {
 			var data=data.Data.Data.result;
