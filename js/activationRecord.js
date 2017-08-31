bbd4618e79cdf08541984c57aded6e91481c0663
maxh <wangdf>
2017-08-31 16:34:55 +0800

    no message

--- js/activationRecord.js
+++ js/activationRecord.js
@@ -22,8 +22,8 @@ $(document).ready(function() {
 			if(data.Data.Success==true){
 				var str = template("protocol-temp", {activition: data.Data.Data.result})
 				$(".activationRecord").html(str);
-				$(".activationRecordLi").click(function () {
-			        let id=$(this).attr('id');// 获取id
+				$(".activationRecordLi").on("tap",function () {
+			        var id=$(this).attr('id');// 获取id
 			        localStorage.setItem("ActivityId",id)
 			        window.location.href = "training_con.html?id=" + id;
 			   });
