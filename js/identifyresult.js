633e98420577982c157bbef2e959f6ea5121c167
hongliwang <whl@163.com>
2017-09-12 15:13:13 +0800

    1.公告评论自动换行 2.认证结果最下面的空白 3.认证结果信息，哪个字段为空，那一个字段就不显示了 4.中国台协三个标签不能左右切换 5.认证结果团体和个人分离

--- js/identifyresult.js
+++ js/identifyresult.js
@@ -34,48 +34,92 @@ $(document).ready(function() {
 				        document.getElementById('loadingdiv').style.display = "none"
 				        var baseInfor = data.baseInfor;
 				        var certInfor = data.certInfor;
-						var str = template("identifyResult", baseInfor)
-						$(".identifyResult").html(str);
+				        var type = baseInfor.type;
+				        // console.log(type);
+						var str1 = template("identifyResult1", baseInfor)
+						var str2 = template("identifyResult2", baseInfor)
+						if(type == 1){
+							$(".identifyResult").html(str1);
+						}else if(type == 2){
+							$(".identifyResult").html(str2);
+						}
+						var date = new Date();
+						// var myDate = date.toLocaleDateString()
+						var mon = date.getMonth()  + 1;
+						if(mon <=9){
+							mon = "0" + mon;
+						}
+						var day = date.getDate();
+						if(day <=9){
+							day = "0" + day;
+						}
+						var myDate = date.getFullYear() + "/" + mon + "/" +  day;
+						console.log(myDate);
+						var endTime = baseInfor.endTime.substring(0,10).replace(/-/g,"/");
+						console.log(endTime);
+						if(Date.parse(myDate)-Date.parse(endTime)>0){
+							// console.log("111")
+							$("#memberStatus").html("已过期");
+							$("#memberStatus").css("color","red");
+
+						}
 						//停止
 						var memberNo = baseInfor.userNo;
 						var faceP = baseInfor.facephoto;
+						var memberBirth =baseInfor.birthday
 						var imgFacephoto = document.createElement("img");
-//						    
 
-						    if(!faceP == ''){
+						    if(faceP !== ''){
 						    	if(faceP.indexOf('Face')>-1){
 						    		faceP = faceP.slice(10,faceP.length)
-						            facePic = "http://www.chntkd.org.cn/"+faceP						    		
+						            facePic = "http://www.chntkd.org.cn/"+faceP
 						    		imgFacephoto.setAttribute("src", facePic);
 						    	    imgFacephoto.setAttribute("style", "width:80%");
 						    	}else{
-						    		facePic = "http://www.chntkd.org.cn/"+faceP	
+						    		facePic = "http://www.chntkd.org.cn/"+faceP
 						    		imgFacephoto.setAttribute("src", facePic);
 						    	    imgFacephoto.setAttribute("style", "width:80%");
 						    	}
-						    	 
-						    }else{
+
+						    }else if(faceP == null){
 						    	 imgFacephoto.setAttribute("src", "../images/111.jpg");
 						    	 imgFacephoto.setAttribute("style", "width:80%");
 						    }
+
 						document.getElementById('faceP').appendChild(imgFacephoto);
-						  var divDom = '<h5 class="certList">资格证信息</h5>'
+						if(memberBirth == null){
+						    $("#memberBirth").parent().hide();
+						}else{
+						    $("#memberBirth").parent().show();
+						}
+						var divDom = '<h5 class="certList">资格证信息</h5>'
 						for(i=0;i<certInfor.length;i++){
 							divDom +='<li><div><span class="commonColor">证书编号：</span><span class="commonRight">'+certInfor[i].CertNo+
 						'</span></div><div><span class="commonColor">证书等级：</span><span class="commonRight">'+certInfor[i].CertGrade+
 						'</span></div><div><span class="commonColor">证书类型：</span><span class="commonRight">'+certInfor[i].TypeName+
 						'</span></div><div><span class="commonColor">通过时间：</span><span class="commonRight">'+certInfor[i].PassTime+
-						'</span></div><div><span class="commonColor">说明：</span><span class="commonRight">'+certInfor[i].Memo+
+						'</span></div><div><span class="commonColor">说明：</span><span class="commonRight" id='+i+'>'+certInfor[i].Memo+
 						'</span></div></li>'
-						 document.getElementById('certInfor').innerHTML = divDom ;  
+						 document.getElementById('certInfor').innerHTML = divDom ;
+						 if(certInfor[0].Memo == ""){
+							$("#0").parent().hide();
+						 }else{
+						 	$("#0").parent().show();
+						 }
+						 if(certInfor[1].Memo == ""){
+							$("#1").parent().hide();
+						 }else{
+						 	$("#1").parent().show();
+						 }
 						}
-						  
-						    
+
+
+
 					} else {
 						document.getElementById('loadingdiv').style.display = "none"
 						$(".identifyResult").html('<div style="text-align:center;margin-top:100px;color:#777">暂时没会员信息!</div>');
-					}	
-			
+					}
+
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) {
 			alert(XMLHttpRequest.status);
